fd0930cd39dc431956a4d2572ca6626ca02ec429
Thom Nichols <thom.nichols@voltserver.com>
2016-04-06 15:54:59 -0400

    Install systemd unit file to /usr/local/systemd/system
    Fixed unit file so it will be run @ runlevel 3

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # This should be run as root.
 install network-status-led.sh /usr/local/bin/
-install -m 644 network-status-led.service /etc/systemd/system/
+# Note that /usr/lib/systemd/system doesn't exist by default in the chip distro..
+install -m 755 -d /usr/lib/systemd/system/
+install -m 644 network-status-led.service /usr/lib/systemd/system/
 systemctl daemon-reload
 systemctl enable network-status-led
+systemctl start network-status-led
